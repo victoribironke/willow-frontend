ade2965adeec28218dfe0359bd6420b5dd2cb1bf
Victor Ibironke <victoribironke2019@gmail.com>
2025-02-26 19:43:12 +0100

    fixed lint errors

--- src/app/api/get-access-token/route.ts
+++ src/app/api/get-access-token/route.ts
@@ -1,41 +1,41 @@
-import { REDIRECT_URI } from "@/constants/constants";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const code = req.nextUrl.searchParams.get("code");
-  const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
-  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+  // const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+  // const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+  console.log(code);
 
   try {
-    const res = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic " +
-          Buffer.from(clientId + ":" + clientSecret).toString("base64"),
-      },
-      body: new URLSearchParams({
-        code: code as string,
-        redirect_uri: REDIRECT_URI,
-        grant_type: "authorization_code",
-      }),
-    });
+    // const res = await fetch("https://accounts.spotify.com/api/token", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/x-www-form-urlencoded",
+    //     Authorization:
+    //       "Basic " +
+    //       Buffer.from(clientId + ":" + clientSecret).toString("base64"),
+    //   },
+    //   body: new URLSearchParams({
+    //     code: code as string,
+    //     redirect_uri: REDIRECT_URI,
+    //     grant_type: "authorization_code",
+    //   }),
+    // });
 
-    const { access_token, expires_in } = await res.json();
+    // const { access_token, expires_in } = await res.json();
 
-    const date_ms = new Date().getTime();
-    const spotifyAuthData = {
-      access_token,
-      expires_at: date_ms + expires_in * 1000,
-    };
+    // const date_ms = new Date().getTime();
+    // const spotifyAuthData = {
+    //   access_token,
+    //   expires_at: date_ms + expires_in * 1000,
+    // };
 
-    return NextResponse.json({ data: spotifyAuthData }, { status: 200 });
+    return NextResponse.json({ data: "hi" }, { status: 200 });
   } catch (e) {
     console.log(e);
-    return NextResponse.json(
-      { error: "A server error occured." },
-      { status: 500 }
-    );
+    // return NextResponse.json(
+    //   { error: "A server error occured." },
+    //   { status: 500 }
+    // );
   }
 };
--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,165 +1,80 @@
-import { ENDPOINTS } from "@/constants/constants";
-import {
-  CurrentlyPlayingTrack,
-  RecentlyPlayedTrack,
-  TopArtist,
-  TopTrack,
-} from "@/types/dashboard";
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
-const { access_token: token, expires_at } = JSON.parse(
-  localStorage.getItem("spotify_auth_data")!
-);
+// const { access_token: token, expires_at } = JSON.parse(
+//   localStorage.getItem("spotify_auth_data")!
+// );
 
-const verifyAuthState = () => {
-  const date_ms = new Date().getTime();
+// const verifyAuthState = () => {
+//   const date_ms = new Date().getTime();
 
-  if (date_ms >= expires_at) return false;
+//   if (date_ms >= expires_at) return false;
 
-  return true;
-};
+//   return true;
+// };
 
-export const useGetCurrentlyPlaying = () => {
-  const url = ENDPOINTS.get_currently_playing;
+// export const useGetTopItems = (
+//   type: "artists" | "tracks",
+//   duration: string
+// ) => {
+//   const url = ENDPOINTS.get_top_items(type);
+//   const mapping = {
+//     "4 weeks": "short_term",
+//     "6 months": "medium_term",
+//     "1 year": "long_term",
+//   };
+//   const params = `?limit=50&time_range=${
+//     mapping[duration as keyof typeof mapping]
+//   }`;
 
-  const { error, data, isLoading } = useQuery({
-    queryKey: ["getCurrentlyPlaying"],
-    queryFn: async () => {
-      const isVerified = verifyAuthState();
+//   const { error, data, refetch, isFetching } = useQuery({
+//     queryKey: ["getTopItems"],
+//     queryFn: async () => {
+//       const isVerified = verifyAuthState();
 
-      if (!isVerified) return null;
+//       if (!isVerified) return null;
 
-      const r = await fetch(url, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      let data: CurrentlyPlayingTrack | null;
+//       const r = await fetch(url + params, {
+//         headers: { Authorization: `Bearer ${token}` },
+//       });
 
-      if (r.status === 204) {
-        return null;
-      }
+//       if (r.status === 204) {
+//         return [];
+//       }
 
-      if (!r.ok) {
-        throw new Error(`API error: ${r.status}`);
-      }
+//       if (!r.ok) {
+//         throw new Error(`API error: ${r.status}`);
+//       }
 
-      const res = await r.json();
+//       const res = await r.json();
 
-      if (res.currently_playing_type === "track") {
-        data = {
-          title: res.item.name,
-          artists: res.item.artists.map((a: { name: string }) => a.name),
-          is_local: res.item.is_local,
-          duration: res.item.duration_ms,
-          progress: res.progress_ms,
-          media_cover: {
-            h: res.item.album.images[0].height,
-            w: res.item.album.images[0].width,
-            src: res.item.album.images[0].url,
-          },
-        };
-      } else data = null;
+//       let data: (TopTrack | TopArtist)[];
 
-      return data;
-    },
-  });
+//       if (type === "tracks") {
+//         data = res.items.map((r: any, i: number) => {
+//           return {
+//             artists: r.artists.map((a: any) => a.name),
+//             image_src: r.album.images[0].url,
+//             title: r.name,
+//             pos: i + 1,
+//             album: r.album.name,
+//           } as TopTrack;
+//         });
+//       } else if (type === "artists") {
+//         data = res.items.map((r: any, i: number) => {
+//           return {
+//             artist: r.name,
+//             image_src: r.images[0].url,
+//             pos: i + 1,
+//             genres: r.genres.slice(0, 3),
+//             followers: r.followers.total,
+//           } as TopArtist;
+//         });
+//       } else data = [];
 
-  return { error, data, isLoading };
-};
+//       return data;
+//     },
+//     refetchInterval: false,
+//   });
 
-export const useGetRecentlyPlayed = () => {
-  const url = ENDPOINTS.get_recently_played;
-
-  const { error, data, isLoading } = useQuery({
-    queryKey: ["getRecentlyPlayed"],
-    queryFn: async () => {
-      const isVerified = verifyAuthState();
-
-      if (!isVerified) return null;
-
-      const r = await fetch(url + "?limit=50", {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-
-      if (r.status === 204) {
-        return [];
-      }
-
-      if (!r.ok) {
-        throw new Error(`API error: ${r.status}`);
-      }
-
-      const res = await r.json();
-
-      return res.items as RecentlyPlayedTrack[];
-    },
-  });
-
-  return { error, data, isLoading };
-};
-
-export const useGetTopItems = (
-  type: "artists" | "tracks",
-  duration: string
-) => {
-  const url = ENDPOINTS.get_top_items(type);
-  const mapping = {
-    "4 weeks": "short_term",
-    "6 months": "medium_term",
-    "1 year": "long_term",
-  };
-  const params = `?limit=50&time_range=${
-    mapping[duration as keyof typeof mapping]
-  }`;
-
-  const { error, data, refetch, isFetching } = useQuery({
-    queryKey: ["getTopItems"],
-    queryFn: async () => {
-      const isVerified = verifyAuthState();
-
-      if (!isVerified) return null;
-
-      const r = await fetch(url + params, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-
-      if (r.status === 204) {
-        return [];
-      }
-
-      if (!r.ok) {
-        throw new Error(`API error: ${r.status}`);
-      }
-
-      const res = await r.json();
-
-      let data: (TopTrack | TopArtist)[];
-
-      if (type === "tracks") {
-        data = res.items.map((r: any, i: number) => {
-          return {
-            artists: r.artists.map((a: any) => a.name),
-            image_src: r.album.images[0].url,
-            title: r.name,
-            pos: i + 1,
-            album: r.album.name,
-          } as TopTrack;
-        });
-      } else if (type === "artists") {
-        data = res.items.map((r: any, i: number) => {
-          return {
-            artist: r.name,
-            image_src: r.images[0].url,
-            pos: i + 1,
-            genres: r.genres.slice(0, 3),
-            followers: r.followers.total,
-          } as TopArtist;
-        });
-      } else data = [];
-
-      return data;
-    },
-    refetchInterval: false,
-  });
-
-  return { error, data, refetch, isFetching };
-};
+//   return { error, data, refetch, isFetching };
+// };
