4fc8a19724fb9edd338dc3cf846b5b45b3bda455
Victor Ibironke <victoribironke2019@gmail.com>
2025-03-05 15:47:33 +0100

    Update header links to use defined page constants for improved navigation

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -21,13 +21,19 @@ const Header = () => {
       </div>
 
       <div className="sm:fixed sm:left-1/2 sm:transform sm:-translate-x-1/2 self-center w-fit bg-white border shadow py-2 text-sm px-6 rounded-full flex items-center justify-center gap-6">
-        <Link href="" className="hover:underline">
+        <Link href={PAGES.home} className="hover:underline">
           Home
         </Link>
-        <Link href="" className="hover:underline whitespace-nowrap">
+        <Link
+          href={PAGES.more_about_willow}
+          className="hover:underline whitespace-nowrap"
+        >
           More about Willow
         </Link>
-        <Link href="" className="hover:underline whitespace-nowrap">
+        <Link
+          href={PAGES.auth.register}
+          className="hover:underline whitespace-nowrap"
+        >
           Sign up
         </Link>
       </div>
